9f7a85029ab223960e751391da9cbe0a48a719bf
jayasimha099 <jayasimha1495@gmail.com>
2020-05-12 01:02:10 +0530

    implemented json, ajax for review page

--- project1/application.py
+++ project1/application.py
@@ -1,12 +1,13 @@
 import os
 import sys
 import time
+import json
 
-from flask import Flask, render_template, request, url_for,flash,session,redirect
+
+from flask import Flask, render_template, request, url_for,flash,session,redirect, jsonify
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, desc
-from books_database import *
 from review_database import *
 from register import *
 from sqlalchemy import or_
@@ -14,12 +15,13 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask(_name_)
+
+
+app = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
-
 
 
 # Configure session to use filesystem
@@ -29,11 +31,10 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 engine = create_engine(os.getenv("DATABASE_URL"))
 Session(app)
-db1.init_app(app)
+db.init_app(app)
 
 with app.app_context():
-	db1.create_all()
-
+	db.create_all()
 
 @app.route("/")
 def index():
@@ -60,8 +61,8 @@ def register():
 		else:
 			user = User(Username=username,
                         Email=email, Gender=gender,Password=password,Cpassword=cpassword, Time_registered=time.ctime(time.time()))
-			db1.session.add(user)
-			db1.session.commit()
+			db.session.add(user)
+			db.session.commit()
 			session[username] = request.form['uname']
 			return render_template("userDetails.html")
 	else:
@@ -117,10 +118,10 @@ def logout():
 
 @app.route("/home")
 def userHome():
-    if "username" in session:
-	username = session["username"]
-        return render_template("userDetails.html", username=username, message="Successfully logged in.", heading="Welcome back")
-    return redirect(url_for('index'))
+	if "username" in session:
+		username = session["username"]
+		return render_template("userDetails.html", username=username, message="Successfully logged in.", heading="Welcome back")
+	return redirect(url_for('index'))
 
 
 @app.route("/bookpage/<isbn>", methods = ["POST","GET"])
@@ -131,12 +132,12 @@ def bookpage(isbn):
 	if "username" in session:
 			username = session["username"]
 			user_reviews = reviewRate.query.filter_by(Isbn = book_isbn).all()
-			res = requests.get("https://www.goodreads.com/book/review_counts.json",
+			res = request.get("https://www.goodreads.com/book/review_counts.json",
                            params={"key": "fKvLN1nI7uY5XcyXi7VgvQ", "isbns": book_isbn})
 			if request.method == "GET":
 				revie = reviewRate.query.filter(reviewRate.Isbn.like(book_isbn),reviewRate.Username.like(username)).first()
 
-				search_result = Books.query.filter_by(isbn=book_isbn).all()
+				search_result = books.query.filter_by(isbn=book_isbn).all()
 
 				if revie is None:
 					return render_template("bookpage.html",book=search_result,res=res,review = user_reviews,username = username)
@@ -149,7 +150,7 @@ def bookpage(isbn):
 
 				isbn = book_isbn
 				username = username
-				
+
 				user = reviewRate(Isbn=book_isbn,Review = reviews, Rating=rating,Username = username)
 				db.session.add(user)
 				db.session.commit()
@@ -157,3 +158,68 @@ def bookpage(isbn):
 				return render_template("bookpage.html",res=res,book=search_result,review=user_reviews,property="none",message="You reviewed this book")
 	else:
 			return redirect(url_for("/"))
+
+@app.route('/books/<id>', methods=['POST','GET'])
+def books(id):
+    try:
+        user = session["email"]
+        result = db.session.query(books).filter(books.isbn == id).first()
+        data=reviewRate.query.all()
+        r=reviewRate.query.filter_by(isbn=id).all()
+        if request.method =='POST':
+            reviewdata = reviewRate(id,user,request.form['comment'], request.form['rating'])
+            user = reviewRate.query.filter_by(email = user, isbn = id).first()
+            data = reviewRate.query.all()
+            if user is not None:
+                print("user has already given rating")
+                var1 = "Error : User has already given rating"
+                return render_template("Book_page.html",user = user, Book_details = result,var1=var1, comments=r, allreviewdata = data)
+                db.session.add(reviewdata)
+                db.session.commit()
+                var1 = "Review Submitted"
+                flash(var1)
+                return redirect(url_for('books',id=id))
+            
+            else:
+                return render_template("Book_page.html",user = user, Book_details = result, comments=r, allreviewdata = data)
+    
+    except Exception as e:
+        print(e)
+        var1 = "You must login to view the homepage"
+        return render_template("reg.html", var1=var1)
+
+@app.route('/api/submitReview', methods=['POST'])
+def submitreview():
+    if not request.is_json:
+        message="Invalid request format"
+        return jsonify(message),400
+    isbn = request.args.get('isbn')
+    try:
+        result = db.session.query(books).filter(books.isbn == isbn).first()
+    except:
+        message="please try again later"
+        return jsonify(message),500
+    if result is None:
+        message="please enter valid isbn"
+        return jsonify(message),404
+    rating = request.get_json()['rating']
+    comment=request.get_json()['comment']
+    email = request.get_json()['email']
+    user = reviewRate.query.filter_by(email=email, isbn=isbn).first()
+    if user is not None:
+        message = "Sorry, you cannot review this book again"
+        return jsonify(message),409
+    reviewdata=reviewRate(isbn=isbn,email=email,comment=comment,rating=rating)
+    try:
+        db.session.add(reviewdata)
+        db.session.commit()
+    except:
+        message="please try again"
+        return jsonify(message),500
+    message = "Review Submitted successfully"
+    return jsonify(message),200
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
